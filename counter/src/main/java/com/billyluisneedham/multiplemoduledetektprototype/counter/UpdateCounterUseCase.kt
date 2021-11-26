0f1bf380c5b8093028ffa9373549e0d49dbaf20b
Billy Needham <56001580+BillyLuisNeedham@users.noreply.github.com>
2021-11-26 16:46:35 +0000

    UpdateCounterUseCase.kt build out

--- counter/src/main/java/com/billyluisneedham/multiplemoduledetektprototype/counter/UpdateCounterUseCase.kt
+++ counter/src/main/java/com/billyluisneedham/multiplemoduledetektprototype/counter/UpdateCounterUseCase.kt
@@ -3,12 +3,24 @@ package com.billyluisneedham.multiplemoduledetektprototype.counter
 internal class UpdateCounterUseCase(
     private val isWithinLimits: CountIsWithinLimitsUseCase
 ) {
-
     fun execute(request: Int): Result<Int> {
-        return Result.success(request)
+        return isWithinLimits.execute(request = request)
+            .run {
+                onCompletionWithinLimits(this, request = request)
+            }
+
     }
+
+    private fun onCompletionWithinLimits(withinLimits: Boolean, request: Int): Result<Int> =
+        when (withinLimits) {
+            true -> Result.success(request)
+            false -> Result.failure(OutOfCounterLimits())
+        }
+
 }
 
+internal class OutOfCounterLimits : Exception()
+
 internal interface CountIsWithinLimitsUseCase {
-    fun execute(request: Int): Result<Boolean>
+    fun execute(request: Int): Boolean
 }
--- counter/src/test/java/com/billyluisneedham/multiplemoduledetektprototype/counter/UpdateCounterUseCaseTest.kt
+++ counter/src/test/java/com/billyluisneedham/multiplemoduledetektprototype/counter/UpdateCounterUseCaseTest.kt
@@ -3,7 +3,6 @@ package com.billyluisneedham.multiplemoduledetektprototype.counter
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
-import java.lang.IllegalStateException
 
 
 class UpdateCounterUseCaseTest {
@@ -12,7 +11,7 @@ class UpdateCounterUseCaseTest {
     fun `if CountIsWithinLimitsUseCase is true, returns the request int wrapped in a Result success`() {
         val testInt = 5
         val isWithinLimits = object : CountIsWithinLimitsUseCase {
-            override fun execute(request: Int): Result<Boolean> = Result.success(true)
+            override fun execute(request: Int): Boolean = true
         }
 
         val updateCounterUseCase = UpdateCounterUseCase(isWithinLimits = isWithinLimits)
@@ -22,17 +21,23 @@ class UpdateCounterUseCaseTest {
     }
 
     @Test
-    fun `if CountIsWithinLimitsUseCase returns false, returns an IllegalStateException wrapped in a Result error`() {
+    fun `if CountIsWithinLimitsUseCase returns false, returns an OutOfCounterLimits exception wrapped in a Result failure`() {
         val testInt = 100
+        var isOutOfCounterLimitsException = false
         val isWithinLimits = object : CountIsWithinLimitsUseCase {
-            override fun execute(request: Int): Result<Boolean> = Result.success(false)
+            override fun execute(request: Int): Boolean = false
         }
 
         val updateCounterUseCase = UpdateCounterUseCase(isWithinLimits = isWithinLimits)
 
+        assertThat(isOutOfCounterLimitsException, `is`(false))
         val result = updateCounterUseCase.execute(testInt)
-        assertThat(result, `is`(Result.failure(IllegalStateException())))
-
-
+        result.onFailure {
+            when (it) {
+                is OutOfCounterLimits -> isOutOfCounterLimitsException = true
+            }
+        }
+        assertThat(isOutOfCounterLimitsException, `is`(true))
     }
+
 }
