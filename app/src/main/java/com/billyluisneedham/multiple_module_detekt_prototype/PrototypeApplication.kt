6d3a8768661720e7d59d034f13703c31f52e282b
Billy Needham <56001580+BillyLuisNeedham@users.noreply.github.com>
2021-11-29 10:50:26 +0000

    hilt added to app and root

---
+++ app/src/main/java/com/billyluisneedham/multiple_module_detekt_prototype/PrototypeApplication.kt
@@ -0,0 +1,7 @@
+package com.billyluisneedham.multiple_module_detekt_prototype
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PrototypeApplication: Application()
